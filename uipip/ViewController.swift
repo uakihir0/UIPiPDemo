4673e06064bb39402b3efafa71577d4fc0c64dba
Akihiro Urushiara <a.urusihara@gmail.com>
2021-11-27 18:30:28 +0900

    Initial Commit

---
+++ uipip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uipip
+//
+//  Created by 漆原 明博/ Urushihara Akihiro on 2021/11/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
